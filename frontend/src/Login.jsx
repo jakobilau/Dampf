05dd997203e5f37934092a03c1a0bf3d1eb7e2b6
albKb59 <albert.klingbeil59@gmail.com>
2026-06-01 21:38:20 +0200

    Login-Formular Prototyp

---
+++ frontend/src/Login.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+export default function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError("");
+
+        try {
+            const res = await fetch("/auth/api/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ username, password }),
+            });
+
+            if (!res.ok) {
+                throw new Error("Login failed");
+            }
+
+            const data = await res.json();
+            localStorage.setItem("token", data.token);
+
+
+        } catch (err) {
+            setError("Invalid Login-Data");
+        }
+
+    }
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Passwort"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Login</button>
+            </form>
+            {error && <p>{error}</p>}
+        </div>
+    );
+}
