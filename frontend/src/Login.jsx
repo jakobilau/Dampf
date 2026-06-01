005867b17aeb6c3d17aad525b7a72c36002b4b0b
albKb59 <albert.klingbeil59@gmail.com>
2026-06-02 01:19:19 +0200

    Weiterleitung nach Login zum Dashboard implementiert

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
- 
+import { Link, useNavigate } from "react-router-dom";
+
 export default function Login() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
+
+    const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -25,7 +27,7 @@ export default function Login() {
 
             const data = await res.json();
             localStorage.setItem("token", data.token);
-
+            navigate("/dashboard");
 
         } catch (err) {
             setError("Invalid Login-Data");
