adee481bd2a82a815e3f3a7cd26d514a6f2e8e86
albKb59 <albert.klingbeil59@gmail.com>
2026-06-01 19:45:58 +0200

    Vite PWA Konfiguration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'Dampf',
+        short_name: 'Dampf',
+        description: 'Unsere Webanwendung',
+        theme_color: '#ffffff',
+
+      }
+    })
+  ]
 })
