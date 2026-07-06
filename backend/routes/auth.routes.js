863110cf94ccfc92edb41605671bd0ec596f186d
Joshua IDe <211126104+Joshua-Ide@users.noreply.github.com>
2026-07-06 15:10:28 +0200

    routes fix

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const authController = require("../controllers/auth.controller");
-const authmiddleware = require("../middleware/auth.middleware");
+const auth = require("../middleware/auth.middleware");
 
 router.post("/login", authController.login);
 router.post("/register", authController.register);
-router.get("/me", authmiddleware, authController.me);
-router.post("/logout", auth, controller.logout);
+router.get("/me", auth, authController.me);
+
+router.post("/logout", authController.logout);
 
 module.exports = router;
--- backend/routes/games.routes.js
+++ backend/routes/games.routes.js
@@ -2,16 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const auth = require("../middleware/auth.middleware");
-const admin = require("../middleware/admin.middleware");
 const controller = require("../controllers/games.controller");
 
 // public
 router.get("/", controller.getGames);
 router.get("/:id", controller.getGame);
-
-// admin only funktioniert noch nich 
-router.post("/", auth, admin, controller.createGame);
-router.patch("/:id", auth, admin, controller.updateGame);
-router.delete("/:id", auth, admin, controller.deleteGame);
 
 module.exports = router;
